baade4bcc80c85fc76aa4dfa7ac17cec0fcaf672
Nathan French <nate@cl0d.com>
2019-02-05 23:08:10 -0500

    evhtp updates

---
+++ cpp/evhtp/cmake/include/config.cmake
@@ -0,0 +1,5 @@
+hunter_config(libevhtp 
+    URL "https://github.com/criticalstack/libevhtp/archive/428e04931937b0b3fb462c4ad661d37eeadfd143.tar.gz"
+    SHA1 "e9bdc4b75279c1c1e866785a14bcf41478e66c74"
+)
+#hunter_config(libevhtp GIT_SELF)
